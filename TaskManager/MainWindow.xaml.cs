4427da9b51373953c7f4086a9006dc9c77db999f
Maxim Bogdan <maxim.bogdan@isa.utm.md>
2020-06-29 23:11:22 +0300

    mesage to zeus

---
+++ TaskManager/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using TaskManager.Models;
+using TaskManager.Services;
+
+namespace TaskManager
+{
+     /// <summary>
+     /// Interaction logic for MainWindow.xaml
+     /// </summary>
+     public partial class MainWindow : Window
+     {
+          private readonly string PATH = $"{Environment.CurrentDirectory}\\TaskData.text";
+          private BindingList<TaskModel> _taskData;
+          private FileWorkService _fileWorkService;
+          public MainWindow()
+          {
+               InitializeComponent();
+          }
+
+          private void Window_Loaded(object sender, RoutedEventArgs e)
+          {
+               _fileWorkService = new FileWorkService(PATH);
+               try
+               {
+                    _taskData = _fileWorkService.LoadData();
+               }
+               catch (Exception ex)
+               {
+                    MessageBox.Show(ex.Message);
+                    this.Close();
+               }
+               dgTaskManager.ItemsSource = _taskData;
+               _taskData.ListChanged += TaskData_ListChanged; 
+          }
+
+
+          private void TaskData_ListChanged(object sender, ListChangedEventArgs e)
+          {
+               if (e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemDeleted || e.ListChangedType == ListChangedType.ItemChanged)
+               {
+                    try
+                    {
+                         _fileWorkService.SaveData(sender);
+                    }
+                    catch (Exception ex)
+                    {
+                         MessageBox.Show(ex.Message);
+                         Close();
+                    }
+
+               }
+          }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+               var items = (BindingList<TaskModel>)dgTaskManager.ItemsSource;
+               items.Clear();
+               try
+               {
+                    _fileWorkService.SaveData(items);
+               }
+               catch (Exception ex)
+               {
+                    MessageBox.Show(ex.Message);
+                    Close();
+               }
+
+
+          }
+    }
+}
---
+++ TaskManager/Services/FileWorkService.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Models;
+
+namespace TaskManager.Services
+{
+     class FileWorkService
+     {
+          private readonly string PATH;
+          public FileWorkService (string path)
+          {
+               PATH = path;
+          }
+
+          public BindingList<TaskModel> LoadData()
+          {
+               var FileExists = File.Exists(PATH);
+               if (!FileExists)
+               {
+                    File.CreateText(PATH).Dispose();
+                    return new BindingList<TaskModel>();
+               }
+               using (var reader = File.OpenText(PATH))
+               {
+                    var FileText = reader.ReadToEnd();
+                    return JsonConvert.DeserializeObject<BindingList<TaskModel>>(FileText);
+               }
+          }
+          public void SaveData(object taskData)
+          {
+               using (StreamWriter writer = File.CreateText(PATH))
+               {
+                    string output = JsonConvert.SerializeObject(taskData);
+                    writer.Write(output);
+               }
+          }
+     }
+}
